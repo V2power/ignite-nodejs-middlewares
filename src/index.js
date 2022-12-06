c91dddd62bf4f16e9378319ef29c84ce5ee5f1e2
V2power <vitor5088@gmail.com>
2022-12-06 12:55:24 -0300

    desafio finalizado com todos os testes passando

--- src/index.js
+++ src/index.js
@@ -10,19 +10,79 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({
+      error: "Username not found!"
+    });
+  }
+
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const todoSizeStatus = user.todos < 11 ? true : false;
+
+  if(!user.pro && !todoSizeStatus){
+    return response.status(403).json({
+      error: "You're not PRO and already have 10 To Dos registered!"
+    });
+  }
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({
+      error: "Username not found!"
+    });
+  }
+
+  const isIdValid = validate(id) ? true : false;
+
+  if (!isIdValid) {
+    return response.status(400).json({
+      error: "The ID isn't a valid UUID!"
+    });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({
+      error: "To do not found!"
+    });
+  }
+
+  request.user = user;
+  request.todo = todo;
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user){
+    return response.status(404).json({
+      error: "Username not found!"
+    });
+  }
+  
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
